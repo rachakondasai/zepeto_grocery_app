956420fb03601e423e3e098fb54ddc35cdd780bd
R Sai Prasad <rsaiprasad@Mac.lan>
2025-08-23 01:02:38 +0530

    Add db.js for MongoDB connection

---
+++ utils/db.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error('❌ MONGODB_URI environment variable is missing');
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        dbName: 'villageapp',
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((mongoose) => mongoose);
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
